9623b5be4dccbd80fdf95e71dc84badb97c2789a
Andrew Heinke <andrew.j.heinke@gmail.com>
2016-09-19 00:50:33 -0500

    adding event handler functions

--- main.js
+++ main.js
@@ -1,11 +1,41 @@
 $(document).ready(function() {
   function startGame() {
     var turn = "X";
+    setMessage("Player " + turn + " gets to start.");
 
-    setMessage(turn + " gets to start.");
+    function nextMove(square) {
+      if (square.innerText === "") {
+        square.innerHTML = turn;
+        if (turn === "X") {
+          turn = "O";
+        }
+        else {
+          turn = "X";
+        }
+        setMessage("Player " + turn + " make your move.");
+      }
+      else {
+        setMessage("That square is already taken. Player " + turn + " please select another square.");
+      }
+    }
+
+    $('.square').on('click', function(square) {
+      nextMove(this);
+    });
   }
+
   function setMessage(msg) {
     document.getElementById('message').innerHTML = msg;
   }
+
+  function checkRow(a, b, c, move) {
+    var result = false;
+
+  }
+
+  function getBox(number) {
+    
+  }
+
   startGame();
 });
