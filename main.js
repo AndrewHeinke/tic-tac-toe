c4d1a3e0f0a2807de465968f94dd8b4ba1b85c00
Andrew Heinke <andrew.j.heinke@gmail.com>
2016-09-19 01:07:31 -0500

    check for win function

--- main.js
+++ main.js
@@ -6,13 +6,17 @@ $(document).ready(function() {
     function nextMove(square) {
       if (square.innerText === "") {
         square.innerHTML = turn;
-        if (turn === "X") {
+        if (checkWin(turn)) {
+          setMessage("Congratulations, Player" + turn + " is the winner!");
+        }
+        else if (turn === "X") {
           turn = "O";
+          setMessage("Player " + turn + " make your move.");
         }
         else {
           turn = "X";
+          setMessage("Player " + turn + " make your move.");
         }
-        setMessage("Player " + turn + " make your move.");
       }
       else {
         setMessage("That square is already taken. Player " + turn + " please select another square.");
@@ -25,16 +29,35 @@ $(document).ready(function() {
   }
 
   function setMessage(msg) {
-    document.getElementById('message').innerHTML = msg;
+    $('#message').html(msg);
+  }
+
+  function checkWin(move) {
+    var result = false;
+    if (checkRow(1,2,3,move) ||
+        checkRow(4,5,6,move) ||
+        checkRow(7,8,9,move) ||
+        checkRow(1,4,7,move) ||
+        checkRow(2,5,8,move) ||
+        checkRow(3,6,9,move) ||
+        checkRow(1,5,9,move) ||
+        checkRow(3,5,7,move)) {
+
+        result = true;
+        }
+    return result;
   }
 
   function checkRow(a, b, c, move) {
     var result = false;
+    if (getBox(a) == move && getBox(b) == move && getBox(c) == move) {
+      result = true;
+    }
 
   }
 
   function getBox(number) {
-    
+    return document.getElementById(number).innerText;
   }
 
   startGame();
